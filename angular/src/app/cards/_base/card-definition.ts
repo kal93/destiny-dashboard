7b2049db7449091a09a68c2b9f60734ac13ba369
lax20attack <lax20attack@yahoo.com>
2017-08-05 23:23:09 -0400

    Reorder default dashboard

--- angular/src/app/cards/_base/card-definition.ts
+++ angular/src/app/cards/_base/card-definition.ts
@@ -113,11 +113,11 @@ export class CardDefinitions {
 
     static defaultDashboards: Array<IUserDashboard> = [{
         id: -1, name: "Default Dashboard", cards: [
-            { id: -1, sequence: 2, definitionId: 1, layoutId: 1, definition: CardDefinitions.definitions[1], layout: CardDefinitions.definitions[1].layouts[1] },
-            { id: -2, sequence: 0, definitionId: 4, layoutId: 2, definition: CardDefinitions.definitions[4], layout: CardDefinitions.definitions[4].layouts[2] },
-            { id: -3, sequence: 3, definitionId: 2, layoutId: 0, definition: CardDefinitions.definitions[2], layout: CardDefinitions.definitions[2].layouts[0] },
-            { id: -4, sequence: 1, definitionId: 0, layoutId: 0, definition: CardDefinitions.definitions[0], layout: CardDefinitions.definitions[0].layouts[0] },
-            { id: -5, sequence: 4, definitionId: 3, layoutId: 2, definition: CardDefinitions.definitions[3], layout: CardDefinitions.definitions[3].layouts[2] }
+            { id: -1, sequence: 1, definitionId: 1, layoutId: 1, definition: CardDefinitions.definitions[1], layout: CardDefinitions.definitions[1].layouts[1] },
+            { id: -2, sequence: 2, definitionId: 4, layoutId: 2, definition: CardDefinitions.definitions[4], layout: CardDefinitions.definitions[4].layouts[2] },
+            { id: -4, sequence: 3, definitionId: 0, layoutId: 0, definition: CardDefinitions.definitions[0], layout: CardDefinitions.definitions[0].layouts[0] },
+            { id: -3, sequence: 4, definitionId: 2, layoutId: 1, definition: CardDefinitions.definitions[2], layout: CardDefinitions.definitions[2].layouts[1] },
+            { id: -5, sequence: 5, definitionId: 5, layoutId: 0, definition: CardDefinitions.definitions[5], layout: CardDefinitions.definitions[5].layouts[0] }
         ]
     }, {
         id: -2, name: "News", cards: [
