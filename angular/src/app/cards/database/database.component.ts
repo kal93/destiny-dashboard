f296435664af241ab11178b4ee3d382b1a96cd18
lax20attack <lax20attack@yahoo.com>
2017-09-05 22:46:23 -0400

    Database

--- angular/src/app/cards/database/database.component.ts
+++ angular/src/app/cards/database/database.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MdPaginator } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
 import { DataSource } from '@angular/cdk/collections';
 import { CardComponent } from '../_base/card.component';
@@ -10,15 +11,12 @@ import { DestinyInventoryItemDefinition } from "app/bungie/manifest/interfaces";
 
 import { DestinyMembership, IAccountSummary, ProgressionBase } from 'app/bungie/services/interface.barrel';
 
-
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/startWith';
-import 'rxjs/add/observable/merge';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/observable/fromEvent';
 
 @Component({
   selector: 'dd-database',
@@ -26,17 +24,23 @@ import 'rxjs/add/observable/fromEvent';
   styleUrls: ['../_base/card.component.scss', './database.component.scss']
 })
 export class DatabaseComponent extends CardComponent {
+  @ViewChild(MdPaginator) paginator: MdPaginator;
+
   CARD_DEFINITION_ID = 9;
 
-  manifestInventoryArray = new Array<DestinyInventoryItemDefinition>();
+  manifestInventory = new Array<DestinyInventoryItemDefinition>();
 
   // Filtering
+  searchText: string = "";
   searchTextForm = new FormControl();
-  itemCategories: Array<any> = [{ value: "all", displayValue: "All" }, { value: "primary", displayValue: "Primary Weapons" }];
+  itemTypes: Array<any> = [{ value: "Quest", displayValue: "Quest" }, { value: "primary", displayValue: "Primary Weapons" }];
 
   // Data table
-  displayedColumns = ['name'];
+  displayedColumns = ['icon', 'name', 'type'];
   dataSource: ItemDefinitionDataSource;
+
+  // Paginator
+  PAGE_SIZE: number = 15;
 
   constructor(public domSanitizer: DomSanitizer, private manifestService: ManifestService, public sharedApp: SharedApp) {
     super(sharedApp);
@@ -44,46 +48,68 @@ export class DatabaseComponent extends CardComponent {
     var manifestInventoryMap: Map<number, DestinyInventoryItemDefinition> = this.manifestService.getTableMap("DestinyInventoryItemDefinition");
     manifestInventoryMap.forEach((itemDefinition, itemHash) => {
       itemDefinition.displayProperties.nameLower = itemDefinition.displayProperties.name.toLowerCase();
-      this.manifestInventoryArray.push(itemDefinition);
+      this.manifestInventory.push(itemDefinition);
     });
-
-    this.dataSource = new ItemDefinitionDataSource(this.manifestInventoryArray);
   }
 
   ngOnInit() {
     super.ngOnInit();
-    this.initSearch();
+
+    this.dataSource = new ItemDefinitionDataSource(this.paginator, this.manifestInventory);
+
+    this.searchTextForm.valueChanges.debounceTime(200).distinctUntilChanged().subscribe((newSearchText) => {
+      this.searchText = newSearchText;
+      this.dataSource.filterChange.next({ searchText: this.searchText.toLowerCase(), itemType: "", pageChange: false });
+    });
   }
 
   ngOnDestroy() {
     super.ngOnDestroy();
   }
 
-  initSearch() {
-    this.searchTextForm.valueChanges.debounceTime(400).distinctUntilChanged().subscribe((newSearchText) => {
-      this.dataSource.filter = newSearchText.toLowerCase();
-    });
+  paginatorChanged() {
+    this.dataSource.filterChange.next({ searchText: this.searchText.toLowerCase(), itemType: "", pageChange: true });
+  }
+
+  iconClicked(itemDef: any) {
+    console.log(itemDef);
   }
 }
 
-export class ItemDefinitionDataSource extends DataSource<any> {
-  _filterChange = new BehaviorSubject('');
-  get filter(): string { return this._filterChange.value; }
-  set filter(filter: string) { this._filterChange.next(filter); }
+interface Filter {
+  searchText: string,
+  itemType: string,
+  pageChange: boolean
+}
 
-  inventoryItems: Array<DestinyInventoryItemDefinition>;
-  constructor(inventoryItems: Array<DestinyInventoryItemDefinition>) {
+export class ItemDefinitionDataSource extends DataSource<any> {
+  filterChange = new BehaviorSubject<Filter>({ searchText: "", itemType: "", pageChange: false });
+  filteredInventoryItems = new Array<DestinyInventoryItemDefinition>();
+
+  constructor(private paginator: MdPaginator, private inventoryItems: Array<DestinyInventoryItemDefinition>) {
     super();
-    this.inventoryItems = inventoryItems;
   }
 
   connect(): Observable<DestinyInventoryItemDefinition[]> {
-    return this._filterChange.map(() => {
-      return this.inventoryItems.slice().filter((item: DestinyInventoryItemDefinition) => {
-        if (item.displayProperties.nameLower.indexOf(this.filter.toLowerCase()) == -1)
+    return this.filterChange.map(() => {
+      let filter = this.filterChange.value;
+
+      // Don't recalculate inventory item filter if we're only changing page size
+      if (!filter.pageChange) {
+        this.filteredInventoryItems = this.inventoryItems.filter((item: DestinyInventoryItemDefinition) => {
+
+          // Filter on search text
+          if (item.displayProperties.nameLower.indexOf(filter.searchText) != -1)
+            return true;
+          if (item.itemTypeAndTierDisplayName.toLowerCase().indexOf(filter.searchText) != -1)
+            return true;
+
           return false;
-        return true;
-      });
+        });
+      }
+
+      let startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+      return this.filteredInventoryItems.slice(startIndex, startIndex + this.paginator.pageSize);
     });
   }
 
