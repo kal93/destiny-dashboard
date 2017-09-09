88d01ab1ea764ceb6a7f1326d6c9daeac88616ba
lax20attack <lax20attack@yahoo.com>
2017-09-09 14:24:41 -0400

    Cache d2 membership ids so we aren't requesting all the time

--- angular/src/app/bungie/shared-bungie.service.ts
+++ angular/src/app/bungie/shared-bungie.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpRequestType, HttpService } from '../shared/services/http.service';
 import { DestinyProfileService } from 'app/bungie/services/service.barrel';
+import { SharedApp } from 'app/shared/services/shared-app.service';
 
 import { environment } from '../../environments/environment';
 import { BungieNetUser, DestinyMembership, DestinyMembershipType } from './services/user/user.interface';
@@ -12,7 +13,8 @@ export class SharedBungie {
     public bungieNetUser: BungieNetUser;
     public destinyMemberships: DestinyMembership[];
 
-    constructor(private destinyProfileService: DestinyProfileService, public http: HttpService) { }
+    constructor(private destinyProfileService: DestinyProfileService, public http: HttpService, private sharedApp: SharedApp) {
+    }
 
     getMembershipsForCurrentUser(): Promise<Array<DestinyMembership>> {
         //Get bungie account information, cache for 5 minutes
@@ -20,16 +22,24 @@ export class SharedBungie {
             this.http.getWithCache("https://www.bungie.net/Platform/User/GetMembershipsForCurrentUser/", HttpRequestType.BUNGIE_PRIVILEGED, 180000).then(response => {
                 this.bungieNetUser = response.bungieNetUser;
                 this.destinyMemberships = response.destinyMemberships;
-                // Filter out old destiny memberships (Destiny 1)
 
+                // Filter out destiny 1 memberships
                 let basicProfilePromises = new Array<Promise<any>>();
                 this.destinyMemberships.forEach((membership) => {
-                    let profilePromise = this.destinyProfileService.getBasicProfile(membership).then((response) => {
-                    }).catch((error) => {
-                        // Remove membership
-                        this.destinyMemberships.splice(this.destinyMemberships.indexOf(membership), 1);
-                    });
-                    basicProfilePromises.push(profilePromise);
+                    let membershipLocalStorageId: string = "d2membershipId-" + membership.membershipId;
+                    var membershipIdEntry = this.sharedApp.getLocalStorage(membershipLocalStorageId);
+
+                    // Don't fetch membershipids that we already know are d2. We have to still test d1 accounts since they could upgrade to d2
+                    if (membershipIdEntry == null || membershipIdEntry == "false") {
+                        let profilePromise = this.destinyProfileService.getBasicProfile(membership).then((response) => {
+                            this.sharedApp.setLocalStorage(membershipLocalStorageId, true);
+                        }).catch((error) => {
+                            this.sharedApp.setLocalStorage(membershipLocalStorageId, false);
+                            // If it's not a destiny 2 membership, remove it
+                            this.destinyMemberships.splice(this.destinyMemberships.indexOf(membership), 1);
+                        });
+                        basicProfilePromises.push(profilePromise);
+                    }
                 });
 
                 Promise.all(basicProfilePromises).then(() => {
