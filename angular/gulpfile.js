d48690562b21e1dab0b6dbf5a6d73efd5b3c9174
lax20attack <lax20attack@yahoo.com>
2017-09-09 20:35:22 -0400

    Cordova + gulp build

--- angular/gulpfile.js
+++ angular/gulpfile.js
@@ -1,23 +1,36 @@
 let clean = require('gulp-clean');
+let fs = require("fs");
 let gulp = require('gulp');
 let replace = require('gulp-replace');
 let runSequence = require('run-sequence');
 let shell = require('gulp-shell');
 
+let BUILD_APP = true;
+let BUILD_CORDOVA = false;
+
 // This is main task for production use
 gulp.task('build', function (done) {
     console.log("Starting build");
-    runSequence('version', 'clean', 'aot_compile', 'service-worker', 'publish', function () {
-        done();
-    });
+
+    if (BUILD_APP) {
+        runSequence('version-app', 'clean-app', 'build-app', 'service-worker', 'publish-app', function () {
+            if (BUILD_CORDOVA)
+                this.buildCordova(done);
+            else
+                done();
+        });
+    }
+    else if (BUILD_CORDOVA)
+        this.buildCordova(done);
 });
 
-
-gulp.task('version', shell.task([
+// Increment version of app
+gulp.task('version-app', shell.task([
     'npm version patch'
 ]));
 
-gulp.task('clean', function (done) {
+// Clean all build directories
+gulp.task('clean-app', function (done) {
     // Clean Java dir
     gulp.src('../java/src/main/webapp/*.js').pipe(clean({ force: true }));
     gulp.src('../java/src/main/webapp/*.png').pipe(clean({ force: true }));
@@ -27,22 +40,22 @@ gulp.task('clean', function (done) {
     gulp.src('../java/src/main/webapp/manifest.json').pipe(clean({ force: true }));
     gulp.src('../java/src/main/webapp/index.html').pipe(clean({ force: true }));
 
-    // Clean cordova dir
-    gulp.src('../cordova/www/*').pipe(clean({ force: true }));
-
     done();
 });
 
-gulp.task('aot_compile', shell.task([
+// AoT Compile Angular
+gulp.task('build-app', shell.task([
     // 'ng build --prod --aot --build-optimizer --no-sourcemap --base-href ./'
     'ng build --prod --aot --no-sourcemap --base-href ./'
 ]));
 
+// Add in our server worker after we AoT compile
 gulp.task('service-worker', shell.task([
     'npm run precache'
 ]));
 
-gulp.task('publish', function (done) {
+// Push latest Angular build to Java
+gulp.task('publish-app', function (done) {
     //Copy files to Java directory
     gulp.src('./build/*.js').pipe(gulp.dest('../java/src/main/webapp'));
     gulp.src('./build/*.png').pipe(gulp.dest('../java/src/main/webapp'));
@@ -52,8 +65,46 @@ gulp.task('publish', function (done) {
     gulp.src('./build/manifest.json').pipe(gulp.dest('../java/src/main/webapp'));
     //Defer js
     gulp.src('./build/index.html').pipe(replace('src=', 'defer src=')).pipe(gulp.dest('../java/src/main/webapp'));
+});
 
 
+
+//
+// Build Cordova
+//
+
+function buildCordova(done) {
+    runSequence('version-app', 'clean-cordova', 'build-cordova', 'publish-cordova', function () {
+        done();
+    });
+}
+
+// Set version in cordova config.xml
+gulp.task('version-cordova', function (done) {
+    let packageJson = JSON.parse(fs.readFileSync('./package.json'));
+
+    let version = packageJson.version;
+    console.log(json);
+    console.log(version);
+    done();
+});
+
+
+// Clean all build directories
+gulp.task('clean-cordova', function (done) {
+    // Clean cordova dir
+    gulp.src('../cordova/www/*').pipe(clean({ force: true }));
+
+    done();
+});
+
+// Build Android cordova
+gulp.task('build-cordova'), shell.task([
+    'cordova build android'
+]);
+
+// Push latest Angular build to Cordova
+gulp.task('publish-cordova', function (done) {
     //Copy files to Cordova directory
     gulp.src('./build/*.js').pipe(gulp.dest('../cordova/www'));
     gulp.src('./build/*.png').pipe(gulp.dest('../cordova/www'));
@@ -62,10 +113,5 @@ gulp.task('publish', function (done) {
     gulp.src('./build/favicon.ico').pipe(gulp.dest('../cordova/www'));
     gulp.src('./build/manifest.json').pipe(gulp.dest('../cordova/www'));
     gulp.src('./build/index.html').pipe(gulp.dest('../cordova/www'));
-
 });
 
-
-gulp.task('cordova-build'), shell.task([
-    'cordova build android'
-]);
