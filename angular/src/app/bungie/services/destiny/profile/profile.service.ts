3acdd36e0f753c5c2dab476623abf6bde7b2a3a5
lax20attack <lax20attack@yahoo.com>
2017-09-14 12:09:54 -0400

    Fix duplicate warning message

--- angular/src/app/bungie/services/destiny/profile/profile.service.ts
+++ angular/src/app/bungie/services/destiny/profile/profile.service.ts
@@ -244,7 +244,7 @@ export class DestinyProfileService {
             }
 
             if (missingItemValue)
-                this.sharedApp.showWarning("Some items in your inventory could not be loaded because they're missing from the Bungie database.");
+                this.sharedApp.showInfoOncePerSession("Some items in your inventory could not be loaded because they're missing from the Bungie database.");
 
             return response;
         });
